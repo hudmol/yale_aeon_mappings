75cb53f3ba78ec62a4bb8ce93059009d2d44ec28
Mark Triggs <mark@dishevelled.net>
2019-05-14 16:07:41 +1000

    Rework to avoid the need to override full templates

--- public/assets/inline_request_form.js
+++ public/assets/inline_request_form.js
@@ -33,3 +33,37 @@ $(function() {
 
     window.setup_inline_request_form = setup_inline_request_form;
 });
+
+function apply_request_buttons_to_infinite() {
+    $(document).on('waypointloaded', '.waypoint', function () {
+
+        $(this).find('.information').addClass('row');
+        $(this).find('.information h3').addClass('col-sm-9');
+
+        $(this).find('.infinite-item').each(function () {
+            var section = $(this);
+            var requestButton = $('<div class="col-sm-3"></div>');
+
+            if (section.hasClass('infinite-item-archival-object') &&
+                section.find('.record-type-badge').text().trim()) {
+                // Update our button to contain the right text
+                var container = section.find('.record-type-badge').text();
+
+                var link = $('<a class="btn btn-default btn-sm" ' +
+                             '   style="margin-bottom: 0.5em;"' +
+                             '   href="javascript:void(0);"></a>');
+
+                link.text('Request ' + container.split(",")[0]);
+
+                link.on('click', function () {
+                    setup_inline_request_form(section.data('uri'));
+                });
+
+                requestButton.append(link);
+            }
+
+            section.find('.information').append(requestButton);
+        });
+    });
+
+}
