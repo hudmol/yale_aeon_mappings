9ffee2bf7f5cb68faf04554f93ee9a7334b2ed4b
Payten Giles <payten.giles@gmail.com>
2020-06-18 11:13:58 +1000

    New slugs feature hijacks any path with :foo/:pants, so we need an extra path item in there to avoid clashing

--- public/routes.rb
+++ public/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-  get '/plugin/inline_aeon_request_form', to: 'inline_aeon_request#form'
+  get '/plugin/yale_aeon_mappings/inline_aeon_request_form', to: 'inline_aeon_request#form'
 
 end
