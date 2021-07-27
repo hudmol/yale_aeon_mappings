e25d83dc155576b9ae9c44e2a1ba526cc017f43a
Payten Giles <payten.giles@gmail.com>
2021-07-27 13:15:26 +1000

    Fix up the infinite scroll request form to use the new aeon request form

--- public/assets/inline_request_form.js
+++ public/assets/inline_request_form.js
@@ -1,38 +1,28 @@
 $(function() {
+  var setup_inline_request_form = function(uri) {
+    const modal = $('#inline_request_modal');
+    modal.find('.action-btn').remove();
 
-    var setup_inline_request_form = function(uri) {
-	modal = $('#inline_request_modal');
-	send_btn = modal.find('.action-btn');
-	send_btn.html('Send Request');
-	send_btn.attr('disabled', true);
+    $('#inline-aeon-request-form').html('Retrieving request information ...');
 
-	send_btn.unbind('click').click(function(e) {
-	    modal.find('.page_action, .request').click();
- 	    modal.modal('hide');
-	});
+    modal.modal('show');
 
-	$('#inline-aeon-request-form').html('Retrieving request information ...');
-	modal.modal('show');
+    $.ajax("/plugin/yale_aeon_mappings/inline_aeon_request_form", {
+      data: {
+        uri: uri
+      },
+      type: "GET",
+    }).done(function(form) {
+      const request_form = $('#inline-aeon-request-form');
+      request_form.html(form);
+      new AeonRequestForm(request_form.find('form:first'));
+    }).fail(function() {
+      $('#inline-aeon-request-form').html('Unable to retrieve request information');
+      $('#inline_request_modal').find('.action-btn').attr('disabled', true);
+    });
+  };
 
-	$.ajax("/plugin/yale_aeon_mappings/inline_aeon_request_form", {
-	    data: {
-		uri: uri
-	    },
-	    type: "GET",
-	}).done(function(form) {
-	    request_form = $('#inline-aeon-request-form');
-	    request_form.html(form);
-	    if (!request_form.find('.page_action, .request').attr('disabled')) {
-	        $('#inline_request_modal').find('.action-btn').attr('disabled', false);
-	    }
-	    request_form.find('.page_action, .request').hide();
-	}).fail(function() {
-	    $('#inline-aeon-request-form').html('Unable to retrieve request information');
-	    $('#inline_request_modal').find('.action-btn').attr('disabled', true);
-	});
-    };
-
-    window.setup_inline_request_form = setup_inline_request_form;
+  window.setup_inline_request_form = setup_inline_request_form;
 });
 
 function apply_request_buttons_to_infinite() {
